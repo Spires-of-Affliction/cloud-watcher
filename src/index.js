01b5ac485e4ecfe2d328e33d3a05e700a1a24169
Denis Onder <denis_onder@protonmail.com>
2020-06-09 23:16:25 +0200

    Temporarily disabled removal of old backups and backups are now executed on alert, rather than on every file change

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const WebSocket = require('ws');
 const chokidar = require('chokidar');
-const { copy, readdirSync, removeSync } = require('fs-extra');
+const { copy } = require('fs-extra');
+// const { copy, readdirSync, removeSync } = require('fs-extra');
 
 require('dotenv').config(); // Load .env into process.env
 
@@ -8,7 +9,7 @@ const {
   FOLDER_PATH: folderPath,
   BACKUP_PATH: backupPath,
   SERVER_URL: url,
-  BACKUP_AMOUNT: backupAmount,
+  // BACKUP_AMOUNT: backupAmount,
 } = process.env;
 
 const ws = new WebSocket(url);
@@ -18,37 +19,38 @@ function generateName(stamp = Date.now()) {
   return `${d.getFullYear()}-${d.getMonth()}-${d.getDate()}_${d.getHours()}-${d.getMinutes()}-${d.getSeconds()}-${d.getMilliseconds()}`;
 }
 
-const dirNameToDate = (dirName) => {
-  const [date, time] = dirName.split('_');
+// const dirNameToDate = (dirName) => {
+//   const [date, time] = dirName.split('_');
 
-  return new Date(generateName(`${date}-${time.split('-').join(':')}`));
-};
+//   return new Date(generateName(`${date}-${time.split('-').join(':')}`));
+// };
 
 function backup() {
+  // TODO: Implement removal of old backups
   // Get old backup directories and sort by date
-  const directories = readdirSync(backupPath)
-    .filter((d) => d !== 'desktop.ini')
-    .sort((a, b) => dirNameToDate(b) - dirNameToDate(a));
+  //   const directories = readdirSync(backupPath)
+  //     .filter((d) => d !== 'desktop.ini')
+  //     .sort((a, b) => dirNameToDate(b) - dirNameToDate(a));
 
-  if (directories.length > backupAmount) {
-    const delimiter = directories.length - backupAmount;
+  //   if (directories.length > backupAmount) {
+  //     const delimiter = directories.length - backupAmount;
 
-    for (let i = 0; i < delimiter; i++)
-      removeSync(`${backupPath}\\${directories[i]}`);
-  }
+  //     for (let i = 0; i < delimiter; i++)
+  //       removeSync(`${backupPath}\\${directories[i]}`);
+  //   }
 
-  copy(folderPath, `${backupPath}\\${generateName()}`, () =>
-    console.log('Backup complete.')
-  );
+  const path = `${backupPath}\\${generateName()}`;
+
+  copy(folderPath, path, () => console.log(`Backup complete:\n${path}`));
 }
 
 console.log('Watcher started\nWaiting for backup calls...');
 
 chokidar.watch(folderPath, { ignoreInitial: true }).on('all', (event, file) => {
   file = file.replace(`${folderPath}\\`, '');
-  // console.log(event, file);
 
   if (/(.lnk)|(.tmp)|(.TMP)/g.test(file)) return;
   ws.send(`${event}::${file}`);
-  // backup();
 });
+
+ws.on('message', (msg) => (msg === 'backup' ? backup() : false));
